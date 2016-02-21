620112ec1185b9a7ba84712af843862ca955e19b
FGtatsuro <fujiistorage@gmail.com>
2016-02-21 21:11:05 +0900

    Confirm python-apt is installed.

--- spec/python-requirements_spec.rb
+++ spec/python-requirements_spec.rb
@@ -1,5 +1,9 @@
 require "spec_helper_#{ENV['SPEC_TARGET_BACKEND']}"
 
+describe package('python-apt'), :if => os[:family] == 'debian' do
+  it { should be_installed }
+end
+
 describe package('python-dev'), :if => os[:family] == 'debian' do
   it { should be_installed }
 end
