1cf264839d24cdfc02f75b1a7421089c1669cf53
FGtatsuro <fujiistorage@gmail.com>
2016-02-21 20:48:47 +0900

    Init project(from `cookiecutter gh:FGtatsuro/cookiecutter-ansible-role`)

---
+++ spec/python-requirements_spec.rb
@@ -0,0 +1,9 @@
+require "spec_helper_#{ENV['SPEC_TARGET_BACKEND']}"
+
+describe package('python-dev'), :if => os[:family] == 'debian' do
+  it { should be_installed }
+end
+
+describe package('build-essential'), :if => os[:family] == 'debian' do
+  it { should be_installed }
+end
